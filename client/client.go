d9a11723c9a8d4c32157c33125920d521f555ed6
jack-the-coder <jack.wherry@live.com>
2019-08-06 16:58:11 -0500

    functioning flag parsing

--- client/client.go
+++ client/client.go
@@ -16,7 +16,7 @@ import (
 )
 
 // example command line: ./client -id 88226 -pass 88377671 [-server v.snazz.xyz]
-var id = flag.Int("room", -1, "chat ID number (leave blank for new room)")
+var room = flag.Int("id", -1, "chat ID number (leave blank for new room)")
 var passcode = flag.String("pass", "", "secret key for chat encryption")
 var server = flag.String("server", "v.snazz.xyz", "hostname or IP address of cryptux server")
 
@@ -37,7 +37,7 @@ func generateNonce() [24]byte {
 	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
 		panic(err)
 	}
-	return nonce
+	return nonce // 24 cryptographically random bytes
 }
 
 func encryptMessage(message string, key [32]byte) []byte {
@@ -61,11 +61,12 @@ func main() {
 	flag.Parse()
 	if *passcode == "" {
 		fmt.Println("Please provide a passcode (secret key) to encrypt your session.")
+		fmt.Println("Run ./client -pass=<password>.")
 		os.Exit(1)
 	}
 
 	fmt.Println("Welcome to cryptux.")
-	if *id == -1 {
+	if *room == -1 {
 		fmt.Println("You have not specified a chat ID number, so we'll open a new chat.")
 	}
 	key := generateKey(*passcode)
